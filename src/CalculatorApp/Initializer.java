43e5d7e4cf41f29044f0766ef8247db4b2b98f32
VonCrowler <conta_github@truevoncrowler.anonaddy.com>
2023-08-02 14:32:31 -0400

    First, second and third Project.

---
+++ src/CalculatorApp/Initializer.java
@@ -0,0 +1,13 @@
+package CalculatorApp;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class Initializer {
+
+    public static void main(String[] args) {
+
+        new Calc();
+    }
+}
---
+++ src/ClockAndCalendar/Initiliazer.java
@@ -0,0 +1,9 @@
+package ClockAndCalendar;
+
+public class Initiliazer {
+    public static void main(String[] args) throws InterruptedException {
+
+        new MyFrame();
+
+    }
+}
